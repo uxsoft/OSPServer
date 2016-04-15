2376261700ca4e54c7a19a350949d1a742ff267a
Jan Dryk <me@uxsoft.cz>
2016-04-15 15:01:21 +0200

    tests

---
+++ OSPServerTests/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSPServerTests
+{
+    public class Book
+    {
+        public Book(string path)
+        {
+            Random random = new Random();
+            string contents = File.ReadAllText(path);
+            var words = contents.SplitByWhiteSpace();
+
+            HashSet<string> uniqueWords = new HashSet<string>();
+            foreach (string word in words)
+                uniqueWords.Add(word);
+
+            Count = uniqueWords.Count;
+
+            Parts = words.GroupBy(w => random.Next(10))
+                .Select(g => string.Join(" ", g))
+                .ToList();
+
+        }
+
+        public int Count { get; set; }
+        public IEnumerable<string> Parts { get; set; }
+    }
+}
---
+++ OSPServerTests/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSPServerTests
+{
+    public static class Extensions
+    {
+        public static string[] SplitByWhiteSpace(this string s)
+        {
+            return s.Split(new string[] { " ", "\t", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
